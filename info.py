636a0d612dd0c6c23b94d9c99a786044275266ac
AF <gulabsinghrajput008@gmail.com>
2025-02-16 12:34:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -156,7 +156,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = True
-URL = environ.get("URL", "https://open-charmain-umesh4q4w6-bfa27389.koyeb.app/")
+URL = environ.get("URL", "https://umesh-bot-411892461e1f.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
